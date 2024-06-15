6db4300bd406b5d240069c41185dfd7ea9cb05d5
Gcvishwas <gcvishwas17@gmail.com>
2024-06-15 09:34:50 -0400

    Changes app js to inlcude budget

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 // App.js
 import React from 'react';
-import Budget from './Budget';
+import Budget from './components/Budget'; // Correct import path
 
 function App() {
   return (
     <div className="App">
-      {/* Budget component */}
       <div className='col-sm'>
         <Budget />
       </div>
