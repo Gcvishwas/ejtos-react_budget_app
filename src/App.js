9246f77c8ae96e8855fbb189ff7d1763baee24d9
Gcvishwas <gcvishwas17@gmail.com>
2024-06-15 09:38:40 -0400

    Imported remaining in App.js and modified the code in ExpenseTotal.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 // App.js
 import React from 'react';
 import Budget from './components/Budget'; // Correct import path
-
+import Remaining from './components/Remaining';
+                 //Remaining component
+                 <div className='col-sm'>
+                 <Remaining />
+             </div>
 function App() {
   return (
     <div className="App">
